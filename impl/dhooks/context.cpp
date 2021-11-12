0bfa7e1f1a32048567260fec98ae5790cbd1f9dc
Ysch <dvvn@ya.ru>
2021-11-12 19:52:11 +0300

    bugfix

--- impl/dhooks/context.cpp
+++ impl/dhooks/context.cpp
@@ -8,9 +8,11 @@
 #include <Windows.h>
 
 #include <vector>
-#include <stdexcept>
 #include <mutex>
 #include <ranges>
+#ifdef _DEBUG
+#include <stdexcept>
+#endif
 
 using namespace dhooks;
 using namespace dhooks::detail;
@@ -103,50 +105,54 @@ status_ex::status_ex(status s) : status_ex_impl{ s }
 }
 #endif
 
-struct context::storage_type : std::vector<element_type>
+struct context::storage_type : std::vector<hook_entry>
 {
 };
 
-static auto _Find_hook_itr(context::storage_type& storage, void* target)
+template <typename T>
+static auto _Find_hook_itr(T& storage, void* target)
 {
-    runtime_assert(target != nullptr);
-    return std::ranges::find(storage, target, &context::element_type::target);
+	runtime_assert(target != nullptr);
+	return std::ranges::find(storage, target, &hook_entry::target);
 }
 
-static hook_entry* _Find_hook(context::storage_type& storage, void* target)
+template <typename T>
+static hook_entry* _Find_hook(T& storage, void* target)
 {
-    auto itr = _Find_hook_itr(storage, target);
-    return itr == storage.end( ) ? nullptr : std::addressof(*itr);
+	auto itr = _Find_hook_itr(storage, target);
+	return itr == storage.end( ) ? nullptr : std::addressof(*itr);
 }
 
-static hook_status _Set_hook_state(context::storage_type& storage, void* target, bool enable)
+template <typename T>
+static hook_status _Set_hook_state(T& storage, void* target, bool enable)
 {
-    const auto entry = _Find_hook(storage, target);
-    if (!entry)
-        return hook_status::ERROR_NOT_CREATED;
+	hook_entry* const entry = _Find_hook(storage, target);
+	if (!entry)
+		return hook_status::ERROR_NOT_CREATED;
 
-    if (entry->enabled( ) == enable)
-        return enable ? hook_status::ERROR_ENABLED : hook_status::ERROR_DISABLED;
+	if (entry->enabled( ) == enable)
+		return enable ? hook_status::ERROR_ENABLED : hook_status::ERROR_DISABLED;
 
-    return entry->set_state(enable);
+	return entry->set_state(enable);
 }
 
-static hook_status _Set_hook_state_all(context::storage_type& storage, bool enable, bool ignore_errors = false)
+template <typename T>
+static hook_status _Set_hook_state_all(T& storage, bool enable, bool ignore_errors = false)
 {
-    //auto frozen = frozen_threads_storage(false);
+	//auto frozen = frozen_threads_storage(false);
 
-    auto storage_active = storage | std::views::filter([](const hook_entry& h)
-    {
-        return h.target( ) != nullptr;
-    });
-    const auto begin = storage_active.begin( );
-    const auto end   = storage_active.end( );
+	auto storage_active = storage | std::views::filter([](const hook_entry& h)
+	{
+		return h.target( ) != nullptr;
+	});
+	const auto begin = storage_active.begin( );
+	const auto end   = storage_active.end( );
 
-    for (auto itr_main = begin; itr_main != end; ++itr_main)
-    {
-        auto& value = *itr_main;
-        if (value.enabled( ) == enable)
-            continue;
+	for (auto itr_main = begin; itr_main != end; ++itr_main)
+	{
+		auto& value = *itr_main;
+		if (value.enabled( ) == enable)
+			continue;
 
 #if 0
 		if (pause_threads)
@@ -157,19 +163,19 @@ static hook_status _Set_hook_state_all(context::storage_type& storage, bool enab
 #endif
 
 #ifdef _DEBUG
-        hook_status main_status = hook_status::UNKNOWN;
-        try
-        {
-            main_status = value.set_state(enable);
-            if (main_status == hook_status::OK)
-                runtime_assert("Error in code");
-            continue;
-        }
-        catch ([[maybe_unused]] const std::runtime_error& e)
-        {
-            if (ignore_errors)
-                continue;
-        }
+		hook_status main_status = hook_status::UNKNOWN;
+		try
+		{
+			main_status = value.set_state(enable);
+			if (main_status == hook_status::OK)
+				runtime_assert("Error in code");
+			continue;
+		}
+		catch ([[maybe_unused]] const std::runtime_error& e)
+		{
+			if (ignore_errors)
+				continue;
+		}
 #else
 		const auto main_status = value.set_state(enable);
 		if (main_status == hook_status::OK)
@@ -178,38 +184,38 @@ static hook_status _Set_hook_state_all(context::storage_type& storage, bool enab
 			continue;
 #endif
 
-        //restore changes back
-        enable = !enable;
-        for (auto itr_child = begin; itr_child != itr_main; ++itr_child)
-        {
-            auto& value_child = *itr_child;
-            if (value_child.enabled( ) == enable)
-                continue;
-            if (const auto temp_status = value_child.set_state(enable); temp_status != hook_status::OK)
-            {
-                runtime_assert("Unable to revert hook state!");
-                return temp_status;
-            }
-        }
-        return main_status;
-    }
+		//restore changes back
+		enable = !enable;
+		for (auto itr_child = begin; itr_child != itr_main; ++itr_child)
+		{
+			auto& value_child = *itr_child;
+			if (value_child.enabled( ) == enable)
+				continue;
+			if (const auto temp_status = value_child.set_state(enable); temp_status != hook_status::OK)
+			{
+				runtime_assert("Unable to revert hook state!");
+				return temp_status;
+			}
+		}
+		return main_status;
+	}
 
-    return hook_status::OK;
+	return hook_status::OK;
 }
 
 //--------
 
 context::context( )
 {
-    storage_ = std::make_unique<storage_type>( );
+	storage_ = std::make_unique<storage_type>( );
 }
 
 context::~context( ) = default;
 
 hook_result context::create_hook(void* target, void* detour)
 {
-    if (!nstd::mem::block(target).executable( ) || !nstd::mem::block(detour).executable( ))
-        return hook_status::ERROR_NOT_EXECUTABLE;
+	if (!nstd::mem::block(target).executable( ) || !nstd::mem::block(detour).executable( ))
+		return hook_status::ERROR_NOT_EXECUTABLE;
 
 #if 0
 	if (storage_->find(target) != nullptr)
@@ -218,85 +224,81 @@ hook_result context::create_hook(void* target, void* detour)
 		return hook_status::ERROR_ALREADY_CREATED;
 #endif
 
-    if (target == detour)
-        return hook_status::ERROR_UNSUPPORTED_FUNCTION;
+	if (target == detour)
+		return hook_status::ERROR_UNSUPPORTED_FUNCTION;
 
-    const auto check_ptr_helper = [&](void* checked)
-    {
-        return checked == target || checked == detour;
-    };
-    for (const auto& value: *storage_)
-    {
-        if (check_ptr_helper(value.target( )) || check_ptr_helper(value.detour( )))
-            return hook_status::ERROR_ALREADY_CREATED;
-    }
+	const auto check_ptr_helper = [&](void* checked)
+	{
+		return checked == target || checked == detour;
+	};
+	for (const auto& value: *storage_)
+	{
+		if (check_ptr_helper(value.target( )) || check_ptr_helper(value.detour( )))
+			return hook_status::ERROR_ALREADY_CREATED;
+	}
 
-    auto ct = trampoline2( );
+	auto new_hook = hook_entry( );
 
-    if (!ct.create(target, detour))
-        return hook_status::ERROR_UNSUPPORTED_FUNCTION;
-    if (!ct.fix_page_protection( ))
-        return hook_status::ERROR_MEMORY_PROTECT;
-
-    auto& new_hook = storage_->emplace_back( );
-
-    static_cast<trampoline2&>(new_hook) = std::move(ct);
+	if (!new_hook.create(target, detour))
+		return hook_status::ERROR_UNSUPPORTED_FUNCTION;
+	if (!new_hook.fix_page_protection( ))
+		return hook_status::ERROR_MEMORY_PROTECT;
 
 #if defined(_M_X64) || defined(__x86_64__)
 	new_hook.detour = ct.pRelay;
 #endif
-    // Back up the target function.
+	// Back up the target function.
 
-    if (new_hook.patch_above( ))
-        new_hook.init_backup((static_cast<LPBYTE>(target) - sizeof(JMP_REL)), sizeof(JMP_REL) + sizeof(JMP_REL_SHORT));
-    else
-        new_hook.init_backup(target, sizeof(JMP_REL));
+	if (new_hook.patch_above( ))
+		new_hook.init_backup((static_cast<LPBYTE>(target) - sizeof(JMP_REL)), sizeof(JMP_REL) + sizeof(JMP_REL_SHORT));
+	else
+		new_hook.init_backup(target, sizeof(JMP_REL));
 
-    hook_result ret(hook_status::OK);
-    ret.entry = std::addressof(new_hook);
+	hook_result ret = hook_status::OK;
+	ret.entry       = std::addressof(storage_->emplace_back(std::move(new_hook)));
 
-    return ret;
+	return ret;
 }
 
 hook_status context::remove_hook(void* target, bool force)
 {
-    const auto entry = _Find_hook(*storage_, target);
-    if (!entry)
-        return hook_status::ERROR_NOT_CREATED;
+	const auto entry = _Find_hook(*storage_, target);
+	if (!entry)
+		return hook_status::ERROR_NOT_CREATED;
 
-    if (entry->enabled( ))
-    {
-        if (const auto status = entry->set_state(false); status != hook_status::OK)
-        {
-            if (!force || status != hook_status::ERROR_MEMORY_PROTECT)
-                return status;
-        }
-        entry->mark_disabled( );
-    }
+	if (entry->enabled( ))
+	{
+		if (const auto status = entry->set_state(false); status != hook_status::OK)
+		{
+			if (!force || status != hook_status::ERROR_MEMORY_PROTECT)
+				return status;
+		}
+		entry->mark_disabled( );
+	}
 
-    storage_->erase(_Find_hook_itr(*storage_, target));
-    return hook_status::OK;
+	storage_->erase(_Find_hook_itr(*storage_, target));
+	return hook_status::OK;
 }
 
 hook_status context::enable_hook(void* target)
 {
-    return _Set_hook_state(*storage_, target, true);
+	return _Set_hook_state(*storage_, target, true);
 }
 
 hook_status context::disable_hook(void* target)
 {
-    return _Set_hook_state(*storage_, target, false);
+	return _Set_hook_state(*storage_, target, false);
 }
 
 hook_result context::find_hook(void* target) const
 {
-    const auto entry = _Find_hook(*storage_, target);
-    if (!entry)
-        return hook_status::ERROR_NOT_CREATED;
+	const auto entry = _Find_hook(*storage_, target);
+	if (!entry)
+		return hook_status::ERROR_NOT_CREATED;
 
-    hook_result result(hook_status::OK);
-    result.entry = /*boost::addressof(entry->second)*/entry;
-    return result;
+	hook_result result(hook_status::OK);
+	result.entry = /*boost::addressof(entry->second)*/entry;
+	return result;
 }
 #if 0
 auto minhook::create_hook_win_api(LPCWSTR pszModule, LPCSTR pszProcName, void* pDetour) -> hook_result
@@ -314,102 +316,103 @@ auto minhook::create_hook_win_api(LPCWSTR pszModule, LPCSTR pszProcName, void* p
 #endif
 void context::remove_all_hooks( )
 {
-    _Set_hook_state_all(*storage_, false, true);
-    storage_->clear( );
+	_Set_hook_state_all(*storage_, false, true);
+	storage_->clear( );
 }
 
 hook_status context::enable_all_hooks( )
 {
-    return _Set_hook_state_all(*storage_, true);
+	return _Set_hook_state_all(*storage_, true);
 }
 
 hook_status context::disable_all_hooks( )
 {
-    return _Set_hook_state_all(*storage_, false);
+	return _Set_hook_state_all(*storage_, false);
 }
 
-struct context_safe::lock_type : std::mutex
+struct context_safe::impl : std::recursive_mutex
 {
+	std::unique_ptr<basic_context> ctx;
 };
 
-template <typename ...Args>
-static auto _Lock_and_work(context_safe::lock_type& mtx, Args&&...args)
+#define LOCK_AND_WORK(_FN_,...) \
+	const auto lock = std::scoped_lock(*impl_);\
+	return impl_->ctx->_FN_(__VA_ARGS__)
+
+context_safe::context_safe(std::unique_ptr<basic_context>&& ctx)
 {
-    const auto lock = std::scoped_lock(mtx);
-    return std::invoke(std::forward<Args>(args)...);
+	runtime_assert(std::dynamic_pointer_cast<context_safe>(ctx) == nullptr);
+	impl_      = std::make_unique<impl>( );
+	impl_->ctx = std::move(ctx);
 }
 
-#define LOCK_AND_WORK(fn,...)\
-	_Lock_and_work(*mtx_, &basic_context::##fn, ctx_,## __VA_ARGS__)
-
-context_safe::context_safe( )
-{
-    mtx_ = std::make_unique<lock_type>( );
-}
+context_safe::~context_safe( )                                 = default;
+context_safe::context_safe(context_safe&&) noexcept            = default;
+context_safe& context_safe::operator=(context_safe&&) noexcept = default;
 
 hook_result context_safe::create_hook(void* target, void* detour)
 {
-    return LOCK_AND_WORK(create_hook, target, detour);
+	LOCK_AND_WORK(create_hook, target, detour);
 }
 
 hook_status context_safe::remove_hook(void* target, bool force)
 {
-    return LOCK_AND_WORK(remove_hook, target, force);
+	LOCK_AND_WORK(remove_hook, target, force);
 }
 
 hook_status context_safe::enable_hook(void* target)
 {
-    return LOCK_AND_WORK(enable_hook, target);
+	LOCK_AND_WORK(enable_hook, target);
 }
 
 hook_status context_safe::disable_hook(void* target)
 {
-    return LOCK_AND_WORK(disable_hook, target);
+	LOCK_AND_WORK(disable_hook, target);
 }
 
 hook_result context_safe::find_hook(void* target) const
 {
-    return LOCK_AND_WORK(find_hook, target);
+	LOCK_AND_WORK(find_hook, target);
 }
 
 void context_safe::remove_all_hooks( )
 {
-    LOCK_AND_WORK(remove_all_hooks);
+	LOCK_AND_WORK(remove_all_hooks);
 }
 
 hook_status context_safe::enable_all_hooks( )
 {
-    return LOCK_AND_WORK(enable_all_hooks);
+	LOCK_AND_WORK(enable_all_hooks);
 }
 
 hook_status context_safe::disable_all_hooks( )
 {
-    return LOCK_AND_WORK(disable_all_hooks);
+	LOCK_AND_WORK(disable_all_hooks);
 }
 
 //--
 
 void current_context::set(std::shared_ptr<basic_context>&& ctx)
 {
-    auto& ref = get_ref( );
-    runtime_assert(ref == nullptr);
-    ref = std::move(ctx);
+	auto& ref = get_ref( );
+	runtime_assert(ref == nullptr);
+	ref = std::move(ctx);
 }
 
 void current_context::reset( )
 {
-    auto& ref = get_ref( );
-    runtime_assert(ref != nullptr);
-    ref.reset( );
+	auto& ref = get_ref( );
+	runtime_assert(ref != nullptr);
+	ref.reset( );
 }
 
 const std::shared_ptr<basic_context>& current_context::get( )
 {
-    return get_ref( );
+	return get_ref( );
 }
 
 std::shared_ptr<basic_context>& current_context::get_ref( )
 {
-    static std::shared_ptr<basic_context> ctx;
-    return ctx;
+	static std::shared_ptr<basic_context> ctx;
+	return ctx;
 }
--- impl/dhooks/context.h
+++ impl/dhooks/context.h
@@ -4,94 +4,92 @@
 
 namespace dhooks
 {
-    enum class hook_status : uint8_t;
-    class hook_entry;
+	enum class hook_status : uint8_t;
+	class hook_entry;
 
-    struct hook_result
-    {
-        template <std::convertible_to<hook_status> T>
-        hook_result(T status)
-            : status(status)
-        {
-            //runtime_assert(this->status == hook_status::OK);
-        }
+	struct hook_result
+	{
+		template <std::convertible_to<hook_status> T>
+		hook_result(T status)
+			: status(status)
+		{
+			//runtime_assert(this->status == hook_status::OK);
+		}
 
-        hook_status status;
-        hook_entry* entry = nullptr;
-    };
+		hook_status status;
+		hook_entry* entry = nullptr;
+	};
 
-    class __declspec(novtable) basic_context
-    {
-    public:
-        virtual ~basic_context( ) = default;
+	class __declspec(novtable) basic_context
+	{
+	public:
+		virtual ~basic_context( ) = default;
 
-        virtual hook_result create_hook(void* target, void* detour) = 0;
-        virtual hook_status remove_hook(void* target, bool force = false) = 0;
-        virtual hook_status enable_hook(void* target) = 0;
-        virtual hook_status disable_hook(void* target) = 0;
-        virtual hook_result find_hook(void* target) const = 0;
-        virtual void remove_all_hooks( ) = 0;
-        virtual hook_status enable_all_hooks( ) = 0;
-        virtual hook_status disable_all_hooks( ) = 0;
-    };
+		virtual hook_result create_hook(void* target, void* detour) = 0;
+		virtual hook_status remove_hook(void* target, bool force = false) = 0;
+		virtual hook_status enable_hook(void* target) = 0;
+		virtual hook_status disable_hook(void* target) = 0;
+		virtual hook_result find_hook(void* target) const = 0;
+		virtual void remove_all_hooks( ) = 0;
+		virtual hook_status enable_all_hooks( ) = 0;
+		virtual hook_status disable_all_hooks( ) = 0;
+	};
 
-    class context final : public basic_context
-    {
-    public:
-        using element_type = hook_entry;
-        struct storage_type;
+	class context final : public basic_context
+	{
+	public:
+		context( );
+		~context( ) override;
 
-        context( );
-        ~context( ) override;
+		hook_result create_hook(void* target, void* detour) override;
+		hook_status remove_hook(void* target, bool force) override;
+		hook_status enable_hook(void* target) override;
+		hook_status disable_hook(void* target) override;
+		hook_result find_hook(void* target) const override;
+		void remove_all_hooks( ) override;
+		hook_status enable_all_hooks( ) override;
+		hook_status disable_all_hooks( ) override;
 
-        hook_result create_hook(void* target, void* detour) override;
-        hook_status remove_hook(void* target, bool force) override;
-        hook_status enable_hook(void* target) override;
-        hook_status disable_hook(void* target) override;
-        hook_result find_hook(void* target) const override;
-        void remove_all_hooks( ) override;
-        hook_status enable_all_hooks( ) override;
-        hook_status disable_all_hooks( ) override;
+	private:
+		struct storage_type;
+		std::unique_ptr<storage_type> storage_;
+	};
 
-    private:
-        std::unique_ptr<storage_type> storage_;
-    };
+	class context_safe final : public basic_context
+	{
+	public:
+		context_safe(std::unique_ptr<basic_context>&& ctx);
+		~context_safe( ) override;
+		context_safe(context_safe&&) noexcept;
+		context_safe& operator=(context_safe&&) noexcept;
 
-    class context_safe final : public basic_context
-    {
-    public:
-        struct lock_type;
+		hook_result create_hook(void* target, void* detour) override;
+		hook_status remove_hook(void* target, bool force) override;
+		hook_status enable_hook(void* target) override;
+		hook_status disable_hook(void* target) override;
+		hook_result find_hook(void* target) const override;
+		void remove_all_hooks( ) override;
+		hook_status enable_all_hooks( ) override;
+		hook_status disable_all_hooks( ) override;
 
-        [[deprecated]]
-        context_safe( );
+	private:
+		struct impl;
+		std::unique_ptr<impl> impl_;
+	};
 
-        hook_result create_hook(void* target, void* detour) override;
-        hook_status remove_hook(void* target, bool force) override;
-        hook_status enable_hook(void* target) override;
-        hook_status disable_hook(void* target) override;
-        hook_result find_hook(void* target) const override;
-        void remove_all_hooks( ) override;
-        hook_status enable_all_hooks( ) override;
-        hook_status disable_all_hooks( ) override;
+	struct current_context
+	{
+		current_context(const current_context& other)                = delete;
+		current_context(current_context&& other) noexcept            = delete;
+		current_context& operator=(const current_context& other)     = delete;
+		current_context& operator=(current_context&& other) noexcept = delete;
 
-    private:
-        context ctx_;
-        std::unique_ptr<lock_type> mtx_;
-    };
-
-    struct current_context
-    {
-        current_context(const current_context& other)                = delete;
-        current_context(current_context&& other) noexcept            = delete;
-        current_context& operator=(const current_context& other)     = delete;
-        current_context& operator=(current_context&& other) noexcept = delete;
-
-        static void set(std::shared_ptr<basic_context>&& ctx);
-        static void reset( );
-        static const std::shared_ptr<basic_context>& get( );
-    private:
-        static std::shared_ptr<basic_context>& get_ref( );
-    };
+		static void set(std::shared_ptr<basic_context>&& ctx);
+		static void reset( );
+		static const std::shared_ptr<basic_context>& get( );
+	private:
+		static std::shared_ptr<basic_context>& get_ref( );
+	};
 
 #if 0
 	/// @brief Creates a Hook for the specified Windows API function, in disabled state.
